614a4d52c33a1baf55b7b0481238da0693ea0570
Tan Vo <tan10tvo@gmail.com>
2021-03-02 21:28:05 -0700

    commit tree dd9b4c1edb817426d5e5bf4b6e0f902c0f74fd9c

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,43 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  pie_flavors = ["chocolate", "vanilla", "strawberry", "peach"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    array = ["wow", "I", "am", "really", "learning"]
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    array = ["I", "am", "really", "learning"]
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+    array = ["chocolate", "vanilla", "strawberry", "arrays!"]
+    array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+    array = ["wow", "vanilla", "strawberry", "peach"]
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+
+    array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    array.first
 end
 
 def retrieve_last_element_from_array(array)
-  
+    array = ["chocolate", "vanilla", "strawberry", "peach", "arrays!"]
+    array.last
 end
